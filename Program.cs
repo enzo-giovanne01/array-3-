e2e342f668219892de86a7add522293aeab08a49
enzo-giovanne01 <giovanneenzo01@gmail.com>
2024-09-01 23:33:23 -0300

    array-3-

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console; 
+int [] numeros = new int[5];
+WriteLine("Digite 5 numeros. ");
+WriteLine("Pressione ENTER para cada número digitado.....");
+for(int n = 0; n < numeros.Length; n++)
+{
+    numeros[n] = Convert.ToInt32(ReadLine());
+}
+Clear();
+WriteLine("---------------------------------------");
+WriteLine("          Estes são os numeros         ");
+WriteLine("---------------------------------------");
+for (int i = 0; i < numeros.Length; i++)
+{
+    WriteLine(numeros[i]);
+}
+ReadKey();
